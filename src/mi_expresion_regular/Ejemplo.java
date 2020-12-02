eb67e963eeadafba370647243689e6743e2ab9e6
Jesús Valido Zafra <jesuszafra22@gmail.com>
2020-12-02 14:08:46 +0100

    Ejercicio de sacar la fecha hecho y comentarios añadidos

--- src/mi_expresion_regular/Ejemplo.java
+++ src/mi_expresion_regular/Ejemplo.java
@@ -6,13 +6,16 @@ public class Ejemplo {
 	
 	public static void main(String[] args) 
 	{
+		//Instanciamos cuatro strings para comprobar
 		String n1 = "Jesús Valido Zafra";
 		String n2 = "León Pérez";
 		String n3 = "Josefa";
 		String n4 = "Colegio Sagrados Corazones SSCC";
-		
+
+		//Instanciamos la expresión regular
 		String regex = "[a-zA-ZáéíóúÁÉÍÓÚ]+\\s[a-zA-ZáéíóúÁÉÍÓÚ]+\\s[a-zA-ZáéíóúÁÉÍÓÚ]+";
 		
+		//Mostramos por pantalla si coincide el patrón con el string
 		System.out.println(Pattern.matches(regex, n1));
 		System.out.println(Pattern.matches(regex, n2));
 		System.out.println(Pattern.matches(regex, n3));
---
+++ src/mi_expresion_regular/Extraer_annio.java
@@ -0,0 +1,30 @@
+package mi_expresion_regular;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Extraer_annio {
+
+	public static void main(String[] args) {
+		
+		//Instanciamos un string con una fecha
+		String fecha = "20/12/2020";
+		
+		//Instanciamos un string con la expresión regular
+		String regex = "\\d{1,2}/\\d{1,2}/(\\d{4})";
+		
+		//Creamos un patrón compilando el string regex
+		Pattern patron = Pattern.compile(regex);
+		
+		//Creamos un matcher usando el método matcher de patron sobre la fecha
+		Matcher matcher = patron.matcher(fecha);
+		
+		//Encontramos la primera aparición del patrón
+		matcher.find();
+		
+		//Mostramos por pantalla el grupo 1
+		System.out.println(matcher.group(1));
+
+	}
+
+}
